ce90b08913c076e499c24a36bc6b1d1b0ce2fdc4
AaronWall97 <aaron.wall@rocketmail.com>
2020-11-06 20:20:03 +0000

    Initial commit

---
+++ placemark-console.console/src/main/kotlin/org/wit/placemark/console/main/main.kt
@@ -0,0 +1,11 @@
+package org.wit.placemark.console.main
+
+import org.wit.placemark.console.controllers.PlacemarkController
+
+fun main(args: Array<String>) {
+    PlacemarkController().start()
+}
+
+
+
+
